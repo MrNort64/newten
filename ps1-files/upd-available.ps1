d6fbc6f1ae28d9cdea6a2e39ec6625c7656cb774
MrNort64 <76720762+MrNort64@users.noreply.github.com>
2020-12-29 21:04:09 -0500

    Delete upd-available.ps1

--- ps1-files/upd-available.ps1
+++
@@ -1,7 +0,0 @@
-[reflection.assembly]::loadwithpartialname('System.Windows.Forms') | Out-Null; >".\content\ps\upd-available.ps1"
-[reflection.assembly]::loadwithpartialname('System.Drawing') | Out-Null; >>".\content\ps\upd-available.ps1"
-$notify = new-object system.windows.forms.notifyicon; >>".\content\ps\upd-available.ps1"
-$notify.icon = [System.Drawing.SystemIcons]::Warning; >>".\content\ps\upd-available.ps1"
-$notify.visible = $true;$notify.showballoontip(10,'Newten','New Update Available!','Warning'); >>".\content\ps\upd-available.ps1"
-Start-Sleep -s 10;
-$notify.Dispose()
